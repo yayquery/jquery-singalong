e97f5dd5729d565c142b509e1de5f25b8afb5edb
yayQuery <yayquery@gmail.com>
2009-11-27 09:59:38 -0500

    adding the two .js files

---
+++ jquery.bouncingball.js
@@ -0,0 +1,386 @@
+
+/*
+ * jQuery Easing v1.3 - http://gsgd.co.uk/sandbox/jquery/easing/
+ *
+ * Uses the built in easing capabilities added In jQuery 1.1
+ * to offer multiple easing options
+ *
+ * TERMS OF USE - jQuery Easing
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright Â© 2008 George McGinley Smith
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+*/
+
+// t: current time, b: begInnIng value, c: change In value, d: duration
+jQuery.easing['jswing'] = jQuery.easing['swing'];
+
+jQuery.extend( jQuery.easing,
+{
+	def: 'easeOutQuad',
+	swing: function (x, t, b, c, d) {
+		//alert(jQuery.easing.default);
+		return jQuery.easing[jQuery.easing.def](x, t, b, c, d);
+	},
+	easeInQuad: function (x, t, b, c, d) {
+		return c*(t/=d)*t + b;
+	},
+	easeOutQuad: function (x, t, b, c, d) {
+		return -c *(t/=d)*(t-2) + b;
+	},
+	easeInOutQuad: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t + b;
+		return -c/2 * ((--t)*(t-2) - 1) + b;
+	},
+	easeInCubic: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t + b;
+	},
+	easeOutCubic: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t + 1) + b;
+	},
+	easeInOutCubic: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t + b;
+		return c/2*((t-=2)*t*t + 2) + b;
+	},
+	easeInQuart: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t + b;
+	},
+	easeOutQuart: function (x, t, b, c, d) {
+		return -c * ((t=t/d-1)*t*t*t - 1) + b;
+	},
+	easeInOutQuart: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t + b;
+		return -c/2 * ((t-=2)*t*t*t - 2) + b;
+	},
+	easeInQuint: function (x, t, b, c, d) {
+		return c*(t/=d)*t*t*t*t + b;
+	},
+	easeOutQuint: function (x, t, b, c, d) {
+		return c*((t=t/d-1)*t*t*t*t + 1) + b;
+	},
+	easeInOutQuint: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return c/2*t*t*t*t*t + b;
+		return c/2*((t-=2)*t*t*t*t + 2) + b;
+	},
+	easeInSine: function (x, t, b, c, d) {
+		return -c * Math.cos(t/d * (Math.PI/2)) + c + b;
+	},
+	easeOutSine: function (x, t, b, c, d) {
+		return c * Math.sin(t/d * (Math.PI/2)) + b;
+	},
+	easeInOutSine: function (x, t, b, c, d) {
+		return -c/2 * (Math.cos(Math.PI*t/d) - 1) + b;
+	},
+	easeInExpo: function (x, t, b, c, d) {
+		return (t==0) ? b : c * Math.pow(2, 10 * (t/d - 1)) + b;
+	},
+	easeOutExpo: function (x, t, b, c, d) {
+		return (t==d) ? b+c : c * (-Math.pow(2, -10 * t/d) + 1) + b;
+	},
+	easeInOutExpo: function (x, t, b, c, d) {
+		if (t==0) return b;
+		if (t==d) return b+c;
+		if ((t/=d/2) < 1) return c/2 * Math.pow(2, 10 * (t - 1)) + b;
+		return c/2 * (-Math.pow(2, -10 * --t) + 2) + b;
+	},
+	easeInCirc: function (x, t, b, c, d) {
+		return -c * (Math.sqrt(1 - (t/=d)*t) - 1) + b;
+	},
+	easeOutCirc: function (x, t, b, c, d) {
+		return c * Math.sqrt(1 - (t=t/d-1)*t) + b;
+	},
+	easeInOutCirc: function (x, t, b, c, d) {
+		if ((t/=d/2) < 1) return -c/2 * (Math.sqrt(1 - t*t) - 1) + b;
+		return c/2 * (Math.sqrt(1 - (t-=2)*t) + 1) + b;
+	},
+	easeInElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return -(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+	},
+	easeOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d)==1) return b+c;  if (!p) p=d*.3;
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		return a*Math.pow(2,-10*t) * Math.sin( (t*d-s)*(2*Math.PI)/p ) + c + b;
+	},
+	easeInOutElastic: function (x, t, b, c, d) {
+		var s=1.70158;var p=0;var a=c;
+		if (t==0) return b;  if ((t/=d/2)==2) return b+c;  if (!p) p=d*(.3*1.5);
+		if (a < Math.abs(c)) { a=c; var s=p/4; }
+		else var s = p/(2*Math.PI) * Math.asin (c/a);
+		if (t < 1) return -.5*(a*Math.pow(2,10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )) + b;
+		return a*Math.pow(2,-10*(t-=1)) * Math.sin( (t*d-s)*(2*Math.PI)/p )*.5 + c + b;
+	},
+	easeInBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*(t/=d)*t*((s+1)*t - s) + b;
+	},
+	easeOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158;
+		return c*((t=t/d-1)*t*((s+1)*t + s) + 1) + b;
+	},
+	easeInOutBack: function (x, t, b, c, d, s) {
+		if (s == undefined) s = 1.70158; 
+		if ((t/=d/2) < 1) return c/2*(t*t*(((s*=(1.525))+1)*t - s)) + b;
+		return c/2*((t-=2)*t*(((s*=(1.525))+1)*t + s) + 2) + b;
+	},
+	easeInBounce: function (x, t, b, c, d) {
+		return c - jQuery.easing.easeOutBounce (x, d-t, 0, c, d) + b;
+	},
+	easeOutBounce: function (x, t, b, c, d) {
+		if ((t/=d) < (1/2.75)) {
+			return c*(7.5625*t*t) + b;
+		} else if (t < (2/2.75)) {
+			return c*(7.5625*(t-=(1.5/2.75))*t + .75) + b;
+		} else if (t < (2.5/2.75)) {
+			return c*(7.5625*(t-=(2.25/2.75))*t + .9375) + b;
+		} else {
+			return c*(7.5625*(t-=(2.625/2.75))*t + .984375) + b;
+		}
+	},
+	easeInOutBounce: function (x, t, b, c, d) {
+		if (t < d/2) return jQuery.easing.easeInBounce (x, t*2, 0, c, d) * .5 + b;
+		return jQuery.easing.easeOutBounce (x, t*2-d, 0, c, d) * .5 + c*.5 + b;
+	}
+});
+
+/*
+ *
+ * TERMS OF USE - EASING EQUATIONS
+ * 
+ * Open source under the BSD License. 
+ * 
+ * Copyright Â© 2001 Robert Penner
+ * All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without modification, 
+ * are permitted provided that the following conditions are met:
+ * 
+ * Redistributions of source code must retain the above copyright notice, this list of 
+ * conditions and the following disclaimer.
+ * Redistributions in binary form must reproduce the above copyright notice, this list 
+ * of conditions and the following disclaimer in the documentation and/or other materials 
+ * provided with the distribution.
+ * 
+ * Neither the name of the author nor the names of contributors may be used to endorse 
+ * or promote products derived from this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY 
+ * EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+ *  COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL,
+ *  EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
+ *  GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED 
+ * AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING
+ *  NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED 
+ * OF THE POSSIBILITY OF SUCH DAMAGE. 
+ *
+ */
+
+
+
+/**
+ * Modded animate() for jQuery
+ * ---------------------------
+ * Allows you to specify additional easing functions on
+ * a per-property basis, E.g.
+ *    $().animate({
+ *       left: 200,
+ *       top: [200, 'easeOutBounce']
+ *    });
+ * ---------------------------
+ * @author James Padolsey
+ * @info 
+ */
+
+
+
+ /*  */
+jQuery.fn.animate = (function(_anim){
+    
+    var jQEasing = jQuery.easing;
+    
+    return function(prop, duration, easing, callback) {
+        
+        var props = {}, optall, i, hasEaser = false;
+        
+        for ( i in prop ) {
+            if ( jQuery.isArray(prop[i]) ) {
+                hasEaser = true;
+                props[i] = prop[i][1];
+                prop[i] = prop[i][0];
+            }
+        }
+        
+        opt = jQuery.speed(duration, easing, callback);
+        
+        if (hasEaser) {
+        
+            opt.step = (function(_step){
+                return function(now, fx) {
+                    var end = fx.end, easeFn;
+                    if ( easeFn = props[fx.prop] ) {
+                        fx.now = jQEasing[easeFn]( now/end, now, 0, end, end );
+                    }
+                    _step && _step.call( fx.elem, fx.now, fx );
+                };
+            })(opt.step);
+            
+        }
+        
+        opt.complete = opt.old || callback || jQuery.isFunction(easing) && easing;
+        
+        return _anim.call( this, prop, opt );
+        
+    };
+    
+})(jQuery.fn.animate);
+
+
+/*  */
+
+
+/*!
+ * jQuery replaceText - v1.1 - 11/21/2009
+ * http://benalman.com/projects/jquery-replacetext-plugin/
+ * 
+ * Copyright (c) 2009 "Cowboy" Ben Alman
+ * Dual licensed under the MIT and GPL licenses.
+ * http://benalman.com/about/license/
+ */
+
+// Project Home - http://benalman.com/projects/jquery-replacetext-plugin/
+//  jQuery('selector').replaceText( search, replace [, text_only ] );
+  
+$.fn.replaceText = function( search, replace, text_only ) {
+    return this.each(function(){
+      var node = this.firstChild,
+        val,
+        new_val,
+        
+        // Elements to be removed at the end.
+        remove = [];
+      
+      // Only continue if firstChild exists.
+      if ( node ) {
+        
+        // Loop over all childNodes.
+        do {
+          
+          // Only process text nodes.
+          if ( node.nodeType === 3 ) {
+            
+            // The original node value.
+            val = node.nodeValue;
+            
+            // The new value.
+            new_val = val.replace( search, replace );
+            
+            // Only replace text if the new value is actually different!
+            if ( new_val !== val ) {
+              
+              if ( !text_only && /</.test( new_val ) ) {
+                // The new value contains HTML, set it in a slower but far more
+                // robust way.
+                $(node).before( new_val );
+                
+                // Don't remove the node yet, or the loop will lose its place.
+                remove.push( node );
+              } else {
+                // The new value contains no HTML, so it can be set in this
+                // very fast, simple way.
+                node.nodeValue = new_val;
+              }
+            }
+          }
+          
+        } while ( node = node.nextSibling );
+      }
+      
+      // Time to remove those elements!
+      remove.length && $(remove).remove();
+    });
+};  
+  
+
+
+function bouncyball(data){
+    
+    var spans = $('span.word:visible'),
+        height= spans.height(),
+        stops = spans.map(function(){
+            return {left: $(this).position().left + $(this).width()/2, 
+              top: $(this).position().top };
+         }).get();
+    
+    $.easing.ball = function (x, t, b, c, d,s) {
+        
+        
+    	// this is the best i got:
+    	//  (Math.sin(t/d*Math.PI)*-height*1/x) + c*b + 100;	
+    		
+        var ret = (Math.sin(t/d*Math.PI)*-height) + b;
+       //console.log(arguments, ret)
+       	return ret;
+    }
+    
+    var $ball = $('#ball').stop().css({top:0,left:0}),
+        stepDuration = (data.stop - data.start) * 1000/stops.length;
+    
+    $.each(stops,function f(k,v){
+      console.log(v)
+      $ball.animate({
+           left: v.left+'px', // 'easeInOutCirc'],
+           top: [v.top+'px', 'ball']
+      },stepDuration);
+    
+    })
+    
+}
+
+$(document).ready(function(){
+    
+    $('#lyrics > span').replaceText(/(\w+)/g,'<span class="word">$1<\/span>' );
+    $(document).bind('show.annotate',function(e,data){
+        bouncyball(data);
+    })
+})
+/*
+
+
+
+$('#ball').animate({
+     left: '-=50px', //, 'ball'],
+     top: ['-=10px', 'ball'] 
+}
+,600)
+
+*/
---
+++ jquery.singalong.js
@@ -0,0 +1,55 @@
+(function($) {
+
+$.fn.singalong = function(options) {
+
+    // build main options before element iteration
+    var opts = $.extend({}, $.fn.annotatevideo.defaults, options);
+    // iterate and reformat each matched element
+    return this.each(function() {
+        
+        if (!opts.elem){   
+            opts.elem = opts.$link.attr('href',opts.uri).text(opts.uri).hide().insertAfter(this);
+        }
+
+        function f(){
+
+            // inverse of: this.currentTime >= opts.start
+            if (this.currentTime < opts.start) return;
+
+            if (this.currentTime >= opts.stop){
+                if (f.stopped) return;
+                f.stopped = true;
+                f.started = false;
+                 $(this).trigger('hide.annotate',{elem: opts.elem});
+                return opts.elem.hide();
+            }
+
+            $(this).trigger('update.annotate',{time:this.currentTime});
+           
+            if (f.started) return;
+            
+            f.started = true;
+            opts.elem.show();
+            $(this).trigger('show.annotate',{elem: opts.elem, start: opts.start, stop : opts.stop});
+        
+        } // eo f()
+
+        $(this).filter('audio,video')
+            .bind('timeupdate',f)
+            .bind('pause stop play',function(){    
+                opts.elem.hide(); f.started = false; f.stopped=false;
+        });
+        
+    }); // this.each
+}; // $.fn.annotatevideo
+
+  $.fn.singalong.defaults = {
+      start : 0,
+      stop   : Infinity,
+      $link  : $('<a>'),
+      uri   : 'http://yayquery.com',
+      elem  : undefined
+  };
+
+})(jQuery);
+
